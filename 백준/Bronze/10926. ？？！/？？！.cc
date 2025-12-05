e817814b8bed533987f04256960ddf52044fea9e
Seonghwan No <seonghwangood01@gmail.com>
2025-12-05 21:05:49 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <array>
+using namespace std;
+
+int main()
+{   
+    string A;
+
+    cin>>A;
+    cout<<A<<"??!";
+
+    return 0;
+}
