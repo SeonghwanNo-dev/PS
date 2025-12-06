63570414947fa66e45a64dfae7b1d3721cc5ce43
Seonghwan No <seonghwangood01@gmail.com>
2025-12-06 16:16:12 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <array>
+using namespace std;
+
+
+int main()
+{   
+
+    int a;
+    int b, c;
+
+    cin >> a;
+
+    for (int i=1; i<=a; i++) 
+    {
+        cin >> b >> c;
+        cout<< "Case #"<< i << ": " << b << " + " << c << " = " << b+c << "\n";
+    } 
+
+    return 0;
+}
+
