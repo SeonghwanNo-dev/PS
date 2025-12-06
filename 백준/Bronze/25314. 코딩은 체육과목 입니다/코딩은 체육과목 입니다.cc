a0c2a180f0d28066b3695bc9210767345652e202
Seonghwan No <seonghwangood01@gmail.com>
2025-12-06 16:01:18 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <array>
+using namespace std;
+
+
+int main()
+{   
+    int a;
+
+    cin >> a;
+    a /=4;
+
+    for (int i=0; i<a; i++) 
+    {
+        cout<<"long ";
+    } 
+    cout<<"int";
+
+    return 0;
+}
+
