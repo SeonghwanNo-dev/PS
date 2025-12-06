9a3e175be3c102eeccb05ea4497fc779dc130d0f
Seonghwan No <seonghwangood01@gmail.com>
2025-12-06 15:50:14 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <array>
+using namespace std;
+
+
+int main()
+{   
+    int a;
+    cin >> a;
+
+    for (int i=0; i<9; i++) { cout << a << " * " << i+1 << " = " << a*(i+1) << endl; } 
+
+    return 0;
+}
+
