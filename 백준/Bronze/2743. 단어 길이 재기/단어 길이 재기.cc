6087add6cef5e83b10aa84ad5f2d935473453b49
Seonghwan No <seonghwangood01@gmail.com>
2025-12-10 12:17:43 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <array>
+using namespace std;
+
+int main()
+{
+    string b;
+    cin >> b;
+    cout<<b.size();
+
+    return 0;
+}
