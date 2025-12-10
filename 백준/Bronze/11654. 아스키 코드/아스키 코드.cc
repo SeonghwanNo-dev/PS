dd9a0bfae4a44512549f9bd60573b2bdac721c74
Seonghwan No <seonghwangood01@gmail.com>
2025-12-10 12:23:39 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <array>
+using namespace std;
+
+int main()
+{
+    char a;
+    cin >> a;
+    cout << (int) a;
+    return 0;
+}
